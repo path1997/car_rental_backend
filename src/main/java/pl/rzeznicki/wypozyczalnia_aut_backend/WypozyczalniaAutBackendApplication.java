e85ce82693a0963281189a8ff49f213e09ca4e6f
path1997 <path1997@users.noreply.github.com>
2023-05-11 21:03:21 +0200

    Initial commit

---
+++ src/main/java/pl/rzeznicki/wypozyczalnia_aut_backend/WypozyczalniaAutBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.rzeznicki.wypozyczalnia_aut_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypozyczalniaAutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WypozyczalniaAutBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rzeznicki/wypozyczalnia_aut_backend/WypozyczalniaAutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rzeznicki.wypozyczalnia_aut_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypozyczalniaAutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
